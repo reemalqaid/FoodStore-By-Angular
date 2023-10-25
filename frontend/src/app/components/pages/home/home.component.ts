c56718df10186d9de006eca6e4d53d0fc28ccd0a
ReemQaid <reemqaid@outlook.sa>
2023-10-25 10:00:34 +0300

    add home page

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+
+export class HomeComponent implements OnInit{
+
+  foods: Food[] = [];
+  constructor( private foodServis:FoodService){
+
+    this.foods = foodServis.getAll();
+
+  }
+
+  ngOnInit():void{
+
+  }
+
+}
